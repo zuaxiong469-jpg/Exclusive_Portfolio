3d4e741eb80e144f4d6b233bbf9919fa067c9a1a
zuaxiong469-jpg <zuaxiong469@gmail.com>
2026-06-02 22:17:49 -0700

    Latest Commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // template_5hw7bwa
 // service_afdj823
 // TJkUKZO8NjfWEWXlh
+const scaleFactor = 1 / 20;
+
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
 
 function contact(event) {
     event.preventDefault();
